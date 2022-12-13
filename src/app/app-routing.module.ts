d7522b8ab2cf4f11949e4743fc5b4e8fe4554ac6
HazarYahyaoui <yahyaoui.hazar@gmail.com>
2022-12-13 20:47:22 +0100

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './component/create-employee/create-employee.component';
+import { EmployeeListComponent } from './component/employee-list/employee-list.component';
+import { Page404Component } from './component/page404/page404.component';
+import { UpdateEmployeeComponent } from './component/update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'list',
+    pathMatch : 'full'
+  },
+  {
+   path:'list',
+   component: EmployeeListComponent
+  },
+  {
+    path:'create',
+    component: CreateEmployeeComponent
+   },
+   {
+    path:'update/:id',
+    component: UpdateEmployeeComponent
+   },
+   {
+    path:'**',
+    component: Page404Component
+   },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { EmployeeListComponent } from './component/employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './component/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './component/update-employee/update-employee.component';
+import { Page404Component } from './component/page404/page404.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    Page404Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
