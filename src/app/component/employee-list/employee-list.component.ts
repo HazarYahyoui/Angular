2c008b5e5a55dd7b210d5c4364d2c6c534bef2a5
HazarYahyaoui <yahyaoui.hazar@gmail.com>
2022-12-15 18:49:18 +0100

    Final Commit

--- src/app/component/employee-list/employee-list.component.ts
+++ src/app/component/employee-list/employee-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit{
 
+  employees : any ;
+  constructor(private employeeService: EmployeeService){ }
+  ngOnInit(): void {
+    this.getAllEmployees();
+  }
+
+  getAllEmployees(){
+    this.employeeService.getAllEmployee().subscribe((response:any)=>{
+     this.employees = response;
+   }, (error)=>{
+     console.log(error)  
+    });
+  }
+
+  deleteEmployee(id:any){
+    this.employeeService.deleteEmployeeById(id).subscribe((response:any)=>{
+      this.getAllEmployees();
+    }, (error)=>{
+      console.log(error)  
+     });
+  }
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment'; 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
+  baseUrl = environment.baseUrl;
   constructor(private http: HttpClient) { }
 
   createEmployee(data : any){
-    return this.http.post('http://localhost:3000/employees', data);
+    return this.http.post(`${this.baseUrl}/employees`, data);
+  }
+
+  getAllEmployee(){
+    return this.http.get(`${this.baseUrl}/employees`);
+  }
+
+  deleteEmployeeById(id: any){
+    return this.http.delete(`${this.baseUrl}/employees/${id}`);
+  }
+
+  getEmployeeById(id:any){
+    return this.http.get(`${this.baseUrl}/employees/${id}`);
+  }
+
+  updateEmployee(id:any, data:any){
+    return this.http.put(`${this.baseUrl}/employees/${id}`, data);
   }
 }
